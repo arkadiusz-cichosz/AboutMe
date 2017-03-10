bf4b9de58ba527b7f61951387e94b0da8444dbe0
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-03-10 12:18:31 +0100

    add commits to portfolio

---
+++ js/githubApi.js
@@ -0,0 +1,20 @@
+$(function() {
+	const portfolioTable = ["Game", "Carousel", "Layout-example", "spellingGame"];
+
+	function getCommit(repoName) {
+		const url = "https://api.github.com/repos/arkadiusz-cichosz/" + repoName + "/commits";
+		$.getJSON(url, function(commits) {
+			const jsonObj = commits;
+			console.log(jsonObj.length);
+			const commitNumber = jsonObj.length;
+			const tag = ".commits #" + repoName;
+			$(tag).text(commitNumber);
+		});
+	}
+
+	setInterval(function() {
+		for(let i = 0; i < portfolioTable.length; i++) {
+			getCommit(portfolioTable[i]);
+		}
+	}, 60000);
+});
