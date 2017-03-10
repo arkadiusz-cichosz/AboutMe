93e6516ef2bd5535e3207d7018d096d78860700b
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-03-10 13:25:17 +0100

    fix bug into commits

--- js/githubApi.js
+++ js/githubApi.js
@@ -5,9 +5,8 @@ $(function() {
 		const url = "https://api.github.com/repos/arkadiusz-cichosz/" + repoName + "/commits";
 		$.getJSON(url, function(commits) {
 			console.log(commits.length);
-			const commitNumber = jsonObj.length;
 			const tag = ".commits #" + repoName;
-			$(tag).text(commitNumber);
+			$(tag).text(commits.length);
 		});
 	}
 
